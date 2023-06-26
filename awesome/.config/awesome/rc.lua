ca9baedb74f7dae6a032ef1d42131e6fa1302ba6
arionrefat <arionrefat@gmail.com>
2023-06-26 13:11:25 +0000

    update

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -403,15 +403,6 @@ globalkeys = mytable.join(
 		os.execute(string.format("amixer -q set %s toggle", beautiful.volume.togglechannel or beautiful.volume.channel))
 		beautiful.volume.update()
 	end, { description = "toggle mute", group = "hotkeys" }),
-	awful.key({ altkey, "Control" }, "m", function()
-		os.execute(string.format("amixer -q set %s 100%%", beautiful.volume.channel))
-		beautiful.volume.update()
-	end, { description = "volume 100%", group = "hotkeys" }),
-	awful.key({ altkey, "Control" }, "0", function()
-		os.execute(string.format("amixer -q set %s 0%%", beautiful.volume.channel))
-		beautiful.volume.update()
-	end, { description = "volume 0%", group = "hotkeys" }),
-
 	-- User programs
 	awful.key({ modkey }, "F2", function()
 		awful.spawn(browser)
